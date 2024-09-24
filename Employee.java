b874b31d7682ff52d027f3689ed40581ffd7753e
anisalisa <anisalisaazhar@gmail.com>
2024-09-24 16:25:00 +0800

    Exercise finally added

--- Employee.java
+++ Employee.java
@@ -4,8 +4,7 @@ public class Employee {
     private double healthAllowance;
     private double transportAllowance;
     private double totalSalary;
-    private double bonus;
-    private double totalAllowances;
+    private double totalBonus;
 
     //constructor
     public Employee(String name, double baseSalary, double healthAllowance, double transportAllowance){
@@ -17,7 +16,7 @@ public class Employee {
     }
     //method for totalSalary
     public double calculateTotalSalary() {
-        double totalSalary = baseSalary + healthAllowance + transportAllowance + bonus;
+        double totalSalary = baseSalary + healthAllowance + transportAllowance + assignBonus();
         return totalSalary;
     }
     //getter for name
@@ -36,11 +35,6 @@ public class Employee {
     public void setBaseSalary (double baseSalary){
         this.baseSalary = baseSalary;
     }
-    //method for allowance
-    public double totalOfAllowances(){
-        double totalAllowances = healthAllowance + transportAllowance + bonus;
-        return totalAllowances;
-    }
     //method for assignBonus
     public double assignBonus(){
         double bonus = 0.0;
@@ -52,7 +46,36 @@ public class Employee {
         } else {
             bonus = 0.0;
         }
-        return bonus;
+        double totalBonus = baseSalary + (bonus * baseSalary);
+        return totalBonus;
+    } 
+    public void displayEmployee(){
+        System.out.println("Employee's name: " + name);
+        System.out.println(name + "'s bonus: RM " + assignBonus());
+        System.out.println(name + "'s total salary: RM " + totalSalary);
+    }
+    public static void main(String[] args) {
+        Employee[] employees = new Employee [5];
+
+        employees[0] = new Employee("Anis", 70000, 890, 890);
+        employees[1] = new Employee("Alisa", 90000, 890, 890);
+        employees[2] = new Employee("Nor", 100000, 890, 890);
+        employees[3] = new Employee("Azhar", 500000, 890, 890);
+        employees[4] = new Employee("Alia", 20000, 890, 890);
+
+        for (Employee employ: employees){
+            employ.displayEmployee();
+        }
+
+        try{
+            int[] arrays = new int[5];
+            System.out.println(arrays[10]);
+        } catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Caught bad "+ e.getMessage());
+        } finally{
+            System.out.println("It will executed anyway");
+        }
+        System.out.println("The program is continue");
+    }
     } 
     
-}
